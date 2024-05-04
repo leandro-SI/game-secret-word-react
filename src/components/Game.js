5b97434bc3b891cf296880f56d8acb3e52f1defd
Leandro <leandro.almeida@uvvnet.com.br>
2024-05-03 21:39:41 -0300

    consumindo states do jogo

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,30 +1,64 @@
+import { useState } from 'react';
 import './Game.css';
 
-const Game = ({ verifyLetter }) => {
+const Game = ({ 
+  verifyLetter, 
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score
+}) => {
+
+  const [letter, setLetter] = useState("")
+
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+
+    
+
+  }
+
   return (
     <div className='game'>
       <p className='points'>
-        Pontuação: 000
+        Pontuação: {score}
       </p>
       <h1>Adivinhe a palavra:</h1>
       <h3 className='tip'>
-        Dica sobre a palavra: <span>Dica...</span>
+        Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
+      <p>Você ainda tem {guesses} tentativas(s).</p>
       <div className='wordContainer'>
-        <span className='letter'>A</span>
-        <span className='blankSquare'></span>
+        {letters.map((letter, i) => (
+          guessedLetters.includes(letter) ? 
+          ( <span key={i} className='letter' >{letter}</span> )
+          : 
+          ( <span key={i} className='blankSquare' ></span> )
+        ))}
       </div>
       <div className='letterContainer'>
         <p>Tente adivinhar uma letra da palavra:</p>
-        <form action="">
-          <input type="text" name='letter' maxLength="1" required />
+        <form onSubmit={handleSubmit} >
+          <input  
+            type="text" 
+            name='letter' 
+            maxLength="1" 
+            required 
+            onChange={(e) => setLetter(e.target.value) }
+            value={letter}
+          />
           <button>Jogar!</button>
         </form>
       </div>
       <div className='wrongLettersContainer'>
         <p>Letras já utilizadas:</p>
-        <span>a,</span>
-        <span>b,</span>
+          {wrongLetters.map((letter, i) => (
+            <span key={i} >{letter}</span>
+          ))}
       </div>
     </div>
   )
